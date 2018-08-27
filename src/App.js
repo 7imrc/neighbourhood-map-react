1ef52d6c479435f5f65990b682d318365211566a
Tim Campbell <tim.campbell@btinternet.com>
2018-08-27 09:13:42 +0100

    feat: add map with marker and InfoWindow
    Google map displaying on screen.
    
    Basic marker and infowindow is displaying using react-google-maps, code
    based from the documentation at:
    https://tomchentw.github.io/react-google-maps/#infowindow

--- src/App.js
+++ src/App.js
@@ -13,7 +13,12 @@ class App extends Component {
       <div className="App">
         <Header />
         <SearchList />
-        <Map />
+        <Map
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRBUF2UVab_IvfyF7rJPQNzWaF8fs-dN8&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <Footer />
       </div>
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+// Code based on that provided by the react-google-map documentation
+// from https://tomchentw.github.io/react-google-maps/#infowindow
+import React from 'react';
+const { compose, withProps, withStateHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} = require("react-google-maps");
 
-class Map extends Component {
-  render() {
-    return (
-      <div className="map">
-        This is my header Component
-      </div>
-    );
-  }
-};
+const Map = compose(
+  withStateHandlers( () => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+      onClick={props.onToggleOpen}
+    >
+      {props.isOpen &&
+        <InfoWindow onCloseClick={props.onToggleOpen}>
+          <div>Contents of InfoWindow</div>
+        </InfoWindow>
+      }
+    </Marker>
+  </GoogleMap>
+);
 
 export default Map;
