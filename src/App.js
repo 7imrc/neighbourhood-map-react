5b3e29ba14fa15c58b511c81682cac515feec7cf
Tim Campbell <tim.campbell@btinternet.com>
2018-08-27 09:30:38 +0100

    feat:  import data from Foursquare
    Fetch of data using FourSquare API.
    
    Resulting search results passed into a venues array in state.
    
    Logging the received request response against the venues array shows
    that the data has imported correctly.
    
    Body in detail

--- src/App.js
+++ src/App.js
@@ -7,6 +7,46 @@ import Footer from './components/Footer';
 
 
 class App extends Component {
+
+  // Set the states for the app.
+    state = {
+      venues: []
+    }
+
+  componentDidMount() {
+      // Get the external data.
+      this.fourSquareLoad();
+    }
+
+    // Fetch the data from FourSquare to populate the map.
+    fourSquareLoad = () => {
+      fetch ("https://api.foursquare.com/v2/venues/search?ll=50.8197675,-1.0879769&intent=browse&radius=30000&query=museum&client_id=HAXM4EPTKFN0LVOHYTA312KBRVX1FXE0SXVFUYCJJKTCYL0J&client_secret=IJC5CLYWJ4R4GDYSUGH3TJ24DP4JAWBKSC2A0GNXJSBVDVCO&v=20180824")
+          .then( (response) => response.json())
+          .then( (response) => {
+            // Error checking to see if received data is in correct format
+            console.log(response.response.venues)
+            return response
+          })
+          // Pass the received data in the correct format to be stored in the empty
+          // venues array in state.
+          .then( (data) => {
+            this.setState({
+              venues: data.response.venues
+            })
+          })
+          // Error checking, log the contents of the venues array, to compare.
+          .then( () => {
+            console.log(this.state.venues)
+          })
+          // If unable to obtain data from Foursquare, alert the user.
+          .catch ( (error) => {
+            alert("There has been a problem trying to get the locations data from Foursquare......please try again");
+          })
+    }
+
+
+
+
   render() {
     return (
       <div className="App">
