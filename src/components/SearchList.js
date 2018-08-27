4e6a2b9222963bdd1f237c2897a5e6303192aea7
Tim Campbell <tim.campbell@btinternet.com>
2018-08-27 22:42:22 +0100

    refactor: SearchList back to Class component
    Made a mistake in trying to change it from a Class component.

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 
-let SearchList = (
+class SearchList extends Component {
+  render() {
+    //console.log('venues array...',this.props.venues);
+    return (
       <div
         className = "search-list"
         style = {{ height: '80%', width: '30%', float: 'left' }}
       >
         <ul>
-          {this.props.venues.map( (venue, index) => {
+          {this.props.venues.map( (venue, index) => (
 
               <li
                 key = {index}
                 onClick = {(e) => this.props.whenMarkerClicked(index, venue.location.lat, venue.location.lng, e)}
               >
                 {venue.name}<br/>{venue.location.lat}<br/>{venue.location.lng}
-             </li>
+              </li>
 
-          })}
+          ))}
         </ul>
       </div>
-);
-
-
+    );
+  }
+};
 
 export default SearchList;
