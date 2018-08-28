1fb179befaac705b7307b43acb4f86d6660b3174
Tim Campbell <tim.campbell@btinternet.com>
2018-08-28 10:15:12 +0100

    feat: filtered search list is now sorted
    The search list names are now sorted alphabetically.

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -10,19 +10,27 @@ class SearchList extends Component {
         //venueSearch: []
     }
 
+  // Update the query state as user enters text in input field.
   updateQuery = (query) => {
     this.setState({ query: query })
   }
 
   render() {
     //console.log('venues array...',this.props.venues);
+
+    // Filter the locations in the search list to those matching locations for
+    // text entered in the input field.
     let showingVenues;
     if (this.state.query) {
       const match = new RegExp(escapeRegExp(this.state.query), 'i');
       showingVenues = this.props.venues.filter( (venue) => match.test(venue.name));
     } else {
-      showingVenues = this.props.venues
+      showingVenues = this.props.venues;
     }
+
+    // sort
+
+    showingVenues.sort(sortBy('name'));
 
     return (
       <div
